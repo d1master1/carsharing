ba428e6d2510d1453e5a5309c1f3fc03407f0aff
d1master1 <barancev2006@gmail.com>
2025-12-18 17:38:18 +0300

    add auth

--- carsharing/urls.py
+++ carsharing/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from cars import views
 
@@ -13,5 +13,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
 
     path('cars/', views.cars_page, name='cars'),
+
+    path('users/', include('users.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import get_user_model, login
+from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from users.forms import CreateUserCreationForm
 
@@ -16,4 +17,25 @@ def register_view(request):
 
         else:
             form = CreateUserCreationForm()
-        return render(request, 'register.html', {f"form": form})
+        return render(request, 'register.html', {"form": form})
+    return None
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Неверный логин или пароль')
+
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
